61747137757307b9a930426ebac9fced1e64770b
salauddin ansari <salauddinansari5499512@gmail.com>
2026-07-29 18:00:46 +0530

    UI: Update Refer and Earn screen with premium referral list and rewards section

--- lib/welvors_home_screen/ui/home/home_screen.dart
+++ lib/welvors_home_screen/ui/home/home_screen.dart
@@ -2,12 +2,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
-import 'package:hugeicons/hugeicons.dart';
 import '../../home_bloc/home_bloc.dart';
 import '../../../onbording_allpage/theme/app_colors.dart';
 
